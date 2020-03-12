2ca5badaa024356e2ebd2b128b69a4afff8660a5
Amber Chan <amber.chan@berkeley.edu>
2020-03-12 17:22:55 +0000

    Completed has_balanced_parens coding challenge

---
+++ balanced-parentheses/balancedparentheses.py
@@ -0,0 +1,46 @@
+"""Does a string have balanced parentheses?
+
+For example::
+
+   >>> has_balanced_parens("()")
+   True
+
+   >>> has_balanced_parens("(Oh Noes!)(")
+   False
+
+   >>> has_balanced_parens("((There's a bonus open paren here.)")
+   False
+
+   >>> has_balanced_parens(")")
+   False
+
+   >>> has_balanced_parens("(")
+   False
+
+   >>> has_balanced_parens("(This has (too many closes.) ) )")
+   False
+
+If you receive a string with no parentheses, consider it balanced::
+
+   >>> has_balanced_parens("Hey...there are no parens here!")
+   True
+"""
+
+
+def has_balanced_parens(phrase):
+    """Does a string have balanced parentheses?"""
+    paren = []
+    for char in phrase:
+      if char == "(":
+        paren.append("(")
+      elif char == ")":
+        if paren:
+          paren.pop()
+        else:
+          return False
+    return False if paren else True
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. YOU CAUGHT ALL THE STRAY PARENS!\n")
